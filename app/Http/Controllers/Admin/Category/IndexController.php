2a51c91e68bc41ddc963db640fd3f486ab74b686
ladiwals <vladjeslav@gmail.com>
2022-05-10 13:46:22 +0300

    Админка \ раздел категорий \ добавил индексный конроллер

---
+++ app/Http/Controllers/Admin/Category/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+class IndexController
+{
+    public function __invoke()
+    {
+        return view('admin.category.index');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,15 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::group(['namespace' => 'Main'], function() {
-    Route::get('', 'IndexController');
+    Route::get('/', 'IndexController');
 });
 
 Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
     Route::group(['namespace' => 'Main'], function() {
-        Route::get('', 'IndexController');
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function() {
+        Route::get('/', 'IndexController');
     });
 });
 
