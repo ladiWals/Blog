da6b6d23e94a40410655095abfffe6e6bc1ba94f
ladiwals <vladjeslav@gmail.com>
2022-05-11 22:53:39 +0300

    Админка \ категории \ добавлена страница детальной информации

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -5,7 +5,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Категории</h1>
+                        <h1 class="m-0">Просмотр - {{ $category->title }}</h1>
                     </div>
                 </div>
             </div>
@@ -15,36 +15,24 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-success mb-3">Добавить</a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
                         <div class="card">
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Название</th>
-                                        <th>Смотреть</th>
-                                    </tr>
-                                    </thead>
                                     <tbody>
-                                    @foreach($categories as $category)
                                         <tr>
+                                            <td>ID</td>
                                             <td>{{ $category->id }}</td>
-                                            <td>{{ $category->title }}</td>
-                                            <td class="col-1"><a href="{{ route('admin.category.show', $category->id) }}"><i class="far fa-eye"></i></a></td>
                                         </tr>
-                                    @endforeach
+                                        <tr>
+                                            <td>Название</td>
+                                            <td>{{ $category->title }}</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
                 </div>
-                {{ $categories->links() }}
             </div>
         </section>
     </div>
